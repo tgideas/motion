fd7f125f9dfd45e374a263dc4f8eba14af778c99
githubFree <github@sina.cn>
2014-12-08 10:13:32 +0800

    修改打包方式为grunt-motion-build
    专门为motion.js订制的打包组件

--- component/Gruntfile.js
+++ component/Gruntfile.js
@@ -1,197 +1,90 @@
-// 包装函数
+/*
+ * grunt-motion-build
+ * https://github.com/aidenxiong/node_modules
+ *
+ * Copyright (c) 2014 Aiden Xiong
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
 module.exports = function(grunt) {
-    var transport = require('grunt-cmd-transport');
-    var style = transport.style.init(grunt);
-    var text = transport.text.init(grunt);
-    var script = transport.script.init(grunt);
-    var seajs = grunt.file.read('sea.js');
-    var footer = '';
-  // 任务配置
+
+  // Project configuration.
   grunt.initConfig({
-        pkg : grunt.file.readJSON("package.json"),
-        transport : {
-            options : {
-                paths : ['src'],
-                parsers : {
-                    '.js' : [script.jsParser],
-                    '.css' : [style.css2jsParser],
-                    '.html' : [text.html2jsParser]
-                }
-            },
-            main : {
-                options : {
-                    idleading : 'dist/main/'
-                },
-                files : [
-                    {
-                        cwd : 'src/main',
-                        src : '**/*',
-                        filter : 'isFile',
-                        dest : '.build/main'
-                    }
-                ]
-            },
-            resource : {
-                options : {
-                    idleading : 'dist/resource/'
-                },
-                files : [
-                    {
-                        cwd : 'src/resource',
-                        src : '**/*',
-                        filter : 'isFile',
-                        dest : '.build/resource'
-                    }
-                ]
-            },
-            dynamicBuild : {
-                options : {
-                    idleading : 'dist/'
-                },
-                files : [
-                    {
-                        cwd : 'src/',
-                        src : ['**/*','**/**/*'],
-                        filter : 'isFile',
-                        dest : '.dynamicBuild/'
-                    }
-                ]
-            }
-        },
-        concat : {
-            main : {
-                options : {
-                    paths : ['./dist/'],
-                    include : 'relative'
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '.build/',
-                        src: ['main/**/*.js'],
-                        dest: 'dist/',
-                        ext: '.js'
-                    }
-                ]
-            },
-            resource : {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '.build/',
-                        src: ['resource/**/*.js'],
-                        dest: 'dist/',
-                        ext: '.js'
-                    }
-                ]
-            },
-            dynamicBuild : {
-                options : {
-                    paths : ['.'],
-                    include : 'relative'
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '.dynamicBuild/dynamicBuild/',
-                        src: ['*.js'],
-                        dest: 'dist/dynamicBuild',
-                        ext: '.js'
-                    }
-                ]
-            }
-        },
-        uglify : {
-            main : {
-                options : {
-                    banner : seajs,
-                    sourceMapIn : function(){ //借用一下sourceMap来获取ID
-                        var id = arguments[0];
-                        footer = 'if(!("mo" in window)){mo={}};seajs.use("'+id+'", function(args){mo["'+id.substring(10, id.lastIndexOf('/'))+'"]=args});';
-                    },
-                    footer : function(){
-                        return '\n'+footer;
-                    }
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/',
-                        src: ['main/**/*.js', '!main/**/*-debug.js'],
-                        dest: 'dist/',
-                        ext: '.js'
-                    }
-                ]
-            },
-            resource : {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/',
-                        src: ['resource/**/*.js', '!resource/**/*-debug.js'],
-                        dest: 'dist/',
-                        ext: '.js'
-                    }
-                ]
-            },
-            dynamicBuild : {
-                options : {
-                    banner : seajs,
-                    sourceMapIn : function(){ //借用一下sourceMap来获取ID
-                        var id = arguments[0];
-                        footer = 'if(!("mo" in window)){mo={}};seajs.use("'+id+'");';
-                    },
-                    footer : function(){
-                        return '\n'+footer;
-                    }
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/',
-                        src: ['dynamicBuild/*.js', '!dynamicBuild/*-debug.js'],
-                        dest: 'dist/',
-                        ext: '.js'
-                    }
-                ]
-            }
-        },
-        clean : {
-            spm : ['.build']
-        }
-    });
-    grunt.loadNpmTasks('grunt-cmd-transport');
-    grunt.loadNpmTasks('grunt-cmd-concat');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    grunt.registerTask('usetheforce_on',
-     'force the force option on if needed', 
-     function() {
-      if ( !grunt.option( 'force' ) ) {
-        grunt.config.set('usetheforce_set', true);
-        grunt.option( 'force', true );
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
       }
-    });
-    grunt.registerTask('usetheforce_restore', 
-      'turn force option off if we have previously set it', 
-      function() {
-      if ( grunt.config.get('usetheforce_set') ) {
-        grunt.option( 'force', false );
+    },
+
+    // Configuration to be run.
+    motion_build: {
+      dev: {
+        options: {
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/main/',
+            filter : 'isFile',
+            src: '**/*',
+            dest: 'build/'
+          }
+        ]
       }
-    });
+    },
+    uglify: {
+      dev : {
+        options:{},
+        files: { 'build/motion.min.js': ['build/motion.js'] }
+      }
+    }
+  });
 
-    grunt.registerTask('setFile','set file name', function(){
-        grunt.config.set('uglify.dynamicBuild.files.0.src.0', 'dynamicBuild/'+arguments[0]);
-    })
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('uglifyCust','custom uglify js', function(){
-        var fileName = arguments[0];
-        // updateConfig(fileName);
-        grunt.registerTask('dynamicBuild',['usetheforce_on','setFile:'+fileName,'transport:dynamicBuild', 'concat:dynamicBuild', 'uglify:dynamicBuild','usetheforce_restore']);
-    })
+  //force the force option on if needed
+  grunt.registerTask('usetheforce_on', 'force the force option on if needed', function() {
+    if ( !grunt.option( 'force' ) ) {
+      grunt.config.set('usetheforce_set', true);
+      grunt.option( 'force', true );
+    }
+  });
 
-    grunt.registerTask('main', ['transport:main', 'concat:main', 'uglify:main', 'clean']);
-    grunt.registerTask('resource', ['transport:resource', 'concat:resource', 'uglify:resource', 'clean']);
-    grunt.registerTask('default', ['transport:main', 'transport:resource', 'concat:main', 'concat:resource', 'uglify:main', 'uglify:resource']);
+  //turn force option off if we have previously set it
+  grunt.registerTask('usetheforce_restore', 'turn force option off if we have previously set it',  function() {
+    if ( grunt.config.get('usetheforce_set') ) {
+      grunt.option( 'force', false );
+    }
+  });
+
+  grunt.registerTask('setFile','set file name', function(file){
+      var fileName = file.replace(/\|/g,'_');
+      var filesSrc = file.split('|').map(function(src){
+        return src + '/' + src + '.js';
+      });
+      var uglifyOpt = {}
+      uglifyOpt['build/'+fileName+'.min.js'] = ['build/'+fileName+'.js'];
+      grunt.config.set('motion_build.dev.files.0.src', filesSrc);
+      grunt.config.set('uglify.dev.files', uglifyOpt);
+  })
+
+  grunt.registerTask('motionCust','custom build motion', function(file){
+      var fileName = file.replace(/\|/g,'_');
+      grunt.registerTask('motionDynamic',['usetheforce_on','setFile:'+file,'motion_build:dev:'+fileName,'usetheforce_restore']);
+      grunt.registerTask('motionDynamicUglify',['usetheforce_on','setFile:'+file,'motion_build:dev:'+fileName,'uglify:dev','usetheforce_restore']);
+  })
+
+  // Actually load this plugin's task(s).
+  grunt.loadNpmTasks('grunt-motion-build');
+
+  grunt.registerTask('motion', ['usetheforce_on','motion_build:dev','usetheforce_restore']);
+  grunt.registerTask('motionUglify', ['usetheforce_on','motion_build:dev','uglify:dev','usetheforce_restore']);
+
 };
--- component/http.js
+++ component/http.js
@@ -5,51 +5,23 @@ var url=require('url');
 var grunt = require('grunt');
 var fs = require('fs');
 var querystring = require("querystring");
-var exec = require("child_process").exec;
 
-function mkdirSync(url,mode,cb){
-    var path = require("path"), arr = url.split("/");
-    mode = mode || 0755;
-    cb = cb || function(){};
-    if(arr[0]==="."){//处理 ./aaa
-        arr.shift();
-    }
-    if(arr[0] == ".."){//处理 ../ddd/d
-        arr.splice(0,2,arr[0]+"/"+arr[1])
-    }
-    function inner(cur){
-        if(!path.existsSync(cur)){//不存在就创建一个
-            fs.mkdirSync(cur, mode)
-        }
-        if(arr.length){
-            inner(cur + "/"+arr.shift());
-        }else{
-            cb();
-        }
-    }
-    arr.length && inner(arr.shift());
-}
 var server = http.createServer(function (request, response) {
+  if(request.url.indexOf('/favicon.ico') != -1){
+    response.end();
+    return;
+  }
    var parsed = url.parse(request.url);
    var query = querystring.parse(parsed.query);
-   var comp = [],debug=false;
-   if(query['u']){ //有u这个参数
-    comp = query['u'].split('|'); 
-    var fileName = comp.join('_')+".js";
-    debug = query['debug'];
-    var str = ''; 
-    str += 'define(function(require, exports, module){\n';
-        for (var i = 0; i < comp.length; i++) {
-            str += '  mo["'+comp[i]+'"] = mo["'+comp[i]+'"] || require("../main/'+comp[i]+'/'+comp[i]+'");\n';
-        };
-    str +='});\n';
-
-    //写入文件
-    mkdirSync('src/dynamicBuild', 0, function(e){
-      fs.writeFile("src/dynamicBuild/"+fileName,str,function(e){
-       grunt.tasks('uglifyCust:'+fileName,{}, function(){
-            grunt.tasks(['dynamicBuild'],{}, function(){
-                fs.readFile("dist/dynamicBuild/"+fileName, function(err, data){
+   var comp = query['u'], debug = query['debug'];
+   var buildTask,filePath;
+   //need package
+   if(comp){ 
+        grunt.tasks('motionCust:'+comp, {}, function(){
+            buildTask = debug == 1 ? 'motionDynamic' : 'motionDynamicUglify';
+            filePath = 'build/'+comp.replace(/\|/g,'_') + (debug == 1 ?'':'.min')+'.js';
+            grunt.tasks(buildTask, {}, function(){
+                fs.readFile(filePath, function(err, data){
                     response.writeHead(200, {
                         'Content-Type': 'application/javascript'
                     });
@@ -58,20 +30,26 @@ var server = http.createServer(function (request, response) {
                     }else{
                         response.write(data);
                     }
-                    //删除构建的临时文件
-                    fs.unlink('src/dynamicBuild/'+fileName, function(){});
-                    fs.unlink('.dynamicBuild/dynamicBuild/'+fileName, function(){});
-                    fs.unlink('.dynamicBuild/dynamicBuild/'+fileName.replace('.js','-debug.js'), function(){});
                     response.end();
-                })
+                });
             })
-        });
-        if(e) throw e;
-      })
-    })
+        })
    }else{
-    response.write('alert("未指明需要用到的组件")')
-    response.end();
+        buildTask = debug == 1 ? 'motion' : 'motionUglify';
+        filePath = 'build/motion' + (debug == 1 ? '' : '.min') + '.js'
+        grunt.tasks(buildTask, {}, function(){
+            fs.readFile(filePath, function(err, data){
+                response.writeHead(200, {
+                    'Content-Type': 'application/javascript'
+                });
+                if(err){
+                    response.write('alert('+JSON.stringify(err).replace(/\\\\/g,'\\')+')');
+                }else{
+                    response.write(data);
+                }
+                response.end();
+            });
+        })
    }
 });
 server.listen(PORT, function(){
