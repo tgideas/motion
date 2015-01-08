fd96331feff76eb8659c586765c6f929ab58aa60
mamboer <mamboer@live.com>
2015-01-08 15:20:03 +0800

    fix api pages' menu

--- grunt/i18n.js
+++ grunt/i18n.js
@@ -8,7 +8,7 @@ module.exports = {
         fixPaths:true,
         allowHtml:true,
         fileFormat: 'json',
-        exclude: ['i18n/','doc/',"assets/","component/","node_modules/"],
+        exclude: ['i18n/',"assets/","component/","node_modules/"],
         locales: ['en'],
         locale: 'zh',
         localesPath: 'locales'
